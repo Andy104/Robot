d6a59a61354910b275881abd3d7374793be73008
Andy104 <32632145+Andy104@users.noreply.github.com>
2017-10-12 16:32:17 +0200

    Revert Revert "qwertyuio"
    This reverts commit 890adb0948bb735bfeeb39aa04a9b236f3c9b501.

---
+++ SW4STM32/Robot/Debug/Drivers/CMSIS/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/Users/Andy/Documents/Robot/Src/system_stm32f3xx.c 
+
+OBJS += \
+./Drivers/CMSIS/system_stm32f3xx.o 
+
+C_DEPS += \
+./Drivers/CMSIS/system_stm32f3xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/CMSIS/system_stm32f3xx.o: C:/Users/Andy/Documents/Robot/Src/system_stm32f3xx.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F303xE -I"C:/Users/Andy/Documents/Robot/Inc" -I"C:/Users/Andy/Documents/Robot/Drivers/STM32F3xx_HAL_Driver/Inc" -I"C:/Users/Andy/Documents/Robot/Drivers/STM32F3xx_HAL_Driver/Inc/Legacy" -I"C:/Users/Andy/Documents/Robot/Drivers/CMSIS/Device/ST/STM32F3xx/Include" -I"C:/Users/Andy/Documents/Robot/Drivers/CMSIS/Include" -I"C:/Users/Andy/Documents/Robot/Inc"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
